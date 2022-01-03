585529015f76cbe7658e0476b995640d7f761c4e
TylerMan <tbaughcome@gmail.com>
2022-01-03 16:51:51 -0500

    Split backend and mobile application into separate repos

--- gazebo/android/app/src/main/kotlin/com/example/gazebo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gazebo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- gazeboemployee/lib/requests/token.dart
+++
@@ -1,9 +0,0 @@
-import "../utilities/localStorage.dart" as localStorage;
-
-Future<String?> getAdminReferralToken() async {
-  try {
-    return await localStorage.readFromLocalStorage("referral_token");
-  } catch (err) {
-    return null;
-  }
-}
