30c4bbdc97c739b53e8fadf660bf75fb22ea1792
TylerMan <tbaughcome@gmail.com>
2022-01-03 16:17:37 -0500

    Small welcome landing for API

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 var express = require("express");
 var app = express();
+const router = express.Router();
 var path = require("path");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
+const cors = require("cors");
 const connectDB = require("./config/db");
 const { header } = require("express-validator");
 app.use(express.json({ extended: false }));
@@ -16,17 +17,26 @@ app.use(
     saveUninitialized: false,
     //cookie: { secure: true }
   })
-  
 );
-connectDB()
+connectDB();
 
-app.use('/api/auth', require('./api/auth'));
-app.use('/api/articles', require('./api/articles'));
-app.use('/api/users', require('./api/users'));
-app.use('/api/uploads', require('./api/uploads'));
+app.use(
+  "/",
+  router.get("/", async (req, res) => {
+    //may want to check those with greatest rate of viewing eventually
+    //Can now just say newest or something
+    try {
+      return res.json({ message: "Welcome to the Rhetorical Gazebo's API!" });
+    } catch (err) {
+      return res.status(500).send("Server error");
+    }
+  })
+);
+app.use("/api/auth", require("./api/auth"));
+app.use("/api/articles", require("./api/articles"));
+app.use("/api/users", require("./api/users"));
+app.use("/api/uploads", require("./api/uploads"));
 const port = process.env.PORT || 3000;
 
 // Start the server
-app.listen(port, () =>
-  console.log(`Gazebo server listening on port ${port}!`)
-);
+app.listen(port, () => console.log(`Gazebo server listening on port ${port}!`));
